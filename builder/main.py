53a9e5052f6afef8246bfed66a8ce25421871175
Florian Siegel <florian1siegel@googlemail.com>
2021-09-05 01:43:06 +0200

    Change tool to clang++

--- builder/main.py
+++ builder/main.py
@@ -30,8 +30,9 @@ backup_cflags = env.get("CFLAGS", [])
 backup_cxxflags = env.get("CXXFLAGS", [])
 
 # Scan for GCC compiler
-env.Tool("gcc")
-env.Tool("g++")
+# env.Tool("gcc")
+# env.Tool("g++")
+env.Tool("clang++")
 
 # Restore C/C++ build flags as they were overridden by env.Tool
 env.Append(CFLAGS=backup_cflags, CXXFLAGS=backup_cxxflags)
